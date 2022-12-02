034450bcf30e870f97ed52fac7d6d50d04e806eb
Alan Gorton <alan@astraappivate.co.uk>
2022-12-02 10:48:52 +0000

    adds FlightUI App

---
+++ FlightUIApp/FlightUIApp/FlightUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightUIAppApp.swift
+//  FlightUIApp
+//
+//  Created by Alan Gorton on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlightUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
