76726b18d822505184114f309f668ec6b6d0ec1f
Kaiman Mehmet <kaiman.mehmet@accenture.com>
2022-12-19 17:42:40 +0000

    Different Menu fields for optional selection, would like to possibly merge these in the future.

--- Sources/FlightUI/Components/MenuField.swift
+++ Sources/FlightUI/Components/MenuField.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-public struct MenuField<SelectionType: CustomStringConvertible & Hashable>: View {
+public struct OptionalMenuField<SelectionType: CustomStringConvertible & Hashable>: View {
     @EnvironmentObject var theme: Theme
     @Binding var selection: SelectionType?
     var options: [SelectionType]
@@ -39,23 +39,59 @@ public struct MenuField<SelectionType: CustomStringConvertible & Hashable>: View
     }
 }
 
+public struct MenuField<SelectionType: CustomStringConvertible & Hashable>: View {
+    @EnvironmentObject var theme: Theme
+    @Binding var selection: SelectionType
+    var options: [SelectionType]
+    
+    public init(selection: Binding<SelectionType>,
+                options: [SelectionType]) {
+        self._selection = selection
+        self.options = options
+    }
+    
+    public var body: some View {
+        Menu {
+            Picker("", selection: $selection) {
+                ForEach(options, id: \.self) { item in
+                    Text("\(item.description)").tag(item)
+                }
+            }
+        } label: {
+            HStack {
+                Text(selection.description)
+                    .typography(.input)
+                Spacer()
+                Image(systemName: "chevron.down")
+                    .foregroundColor(theme.menuFieldAccent)
+                    .fontWeight(.bold)
+                
+            }
+        }
+        .padding()
+        .background(theme.menuFieldBackground)
+        .frame(height: theme.menuFieldHeight)
+        .cornerRadius(theme.menuFieldCornerRadius)
+    }
+}
+
 struct MenuField_Previews: PreviewProvider {
-    @State static var selection: String?
-    @State static var selectionEmpty: String? = ""
+    @State static var optionalSelection: String?
+    @State static var selection: String = "Iron Man"
+    
     static let options = ["Thor", "Iron Man", "Captain America"]
     
     static var previews: some View {
         
         VStack {
-            MenuField(selection: $selection,
+            OptionalMenuField(selection: $optionalSelection,
                       options: options)
             
-            MenuField(selection: $selection,
+            OptionalMenuField(selection: $optionalSelection,
                       options: options,
                       placeholder: "Custom Placeholder Text")
             
-            MenuField(selection: $selectionEmpty,
-                      options: options)
+            MenuField(selection: $selection, options: options)
             
         }
         .padding()
