3fd4d774170e201427297d13196a0a081fca0975
Kaiman Mehmet <kaiman.mehmet@accenture.com>
2022-12-21 12:44:50 +0000

    Full suite of MenuField Tests

--- Tests/FlightUITests/Components/MenuFieldTests.swift
+++ Tests/FlightUITests/Components/MenuFieldTests.swift
@@ -6,10 +6,15 @@ import ViewInspector
 @testable import FlightUI
 
 extension MenuField: Inspectable {}
+extension OptionalMenuField: Inspectable {}
 extension Input: Inspectable {}
+extension EmptyField: Inspectable {}
 
 class MenuFieldTests: XCTestCase {
-    func test_menuField_givenSelectionIronMan_thenLabelIsIronMan() throws {
+    
+    // MARK: - Menu Field -
+    
+    func test_menuField_givenSelection_thenLabelTitleIsSelection() throws {
         // given
         let options = ["Thor", "Iron Man", "Captain America"]
         let selection = Binding<String>(wrappedValue: "Iron Man")
@@ -21,7 +26,7 @@ class MenuFieldTests: XCTestCase {
     }
     
     
-    func test_menuField_givenSelectionIronMan_whenThorSelected_thenCorrectlyUpdatesLabel() throws {
+    func test_menuField_givenSelection_whenSelectionChanged_thenCorrectlyUpdatesLabel() throws {
         // given
         let options = ["Thor", "Iron Man", "Captain America"]
         let selection = Binding<String>(wrappedValue: "Iron Man")
@@ -44,6 +49,98 @@ class MenuFieldTests: XCTestCase {
         // then
         let actualAttributes = try sut.inspect().view(MenuField<Swift.String>.self).menu().labelView().hStack().text(0).attributes()
         let expectedAttributes = try Text("").typography(.input).environmentObject(Theme()).inspect().text(0).attributes()
+        XCTAssertEqual(try actualAttributes.font(), try expectedAttributes.font())
+    }
+    
+    // MARK: - Optional Menu Field -
+    
+    func test_optionalMenuField_givenSelectionNil_thenLabelTitleIsSelect() throws {
+        // given
+        let options = ["Thor", "Iron Man", "Captain America"]
+        let selection = Binding<String?>(wrappedValue: nil)
+        let sut = OptionalMenuField(selection: selection, options: options).environmentObject(Theme())
+        
+        // then
+        let selectionLabelString = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).string()
+        XCTAssertEqual(selectionLabelString, "Select")
+    }
+    
+    func test_optionalMenuField_givenSelectionNilAndPlaceholderGiven_thenLabelTitleIsPlaceholder() throws {
+        // given
+        let options = ["Thor", "Iron Man", "Captain America"]
+        let selection = Binding<String?>(wrappedValue: nil)
+        let sut = OptionalMenuField(selection: selection, options: options, placeholder: "Choose one").environmentObject(Theme())
+        
+        // then
+        let selectionLabelString = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).string()
+        XCTAssertEqual(selectionLabelString, "Choose one")
+    }
+    
+    func test_optionalMenuField_givenSelection_thenLabelTitleIsSelection() throws {
+        // given
+        let options = ["Thor", "Iron Man", "Captain America"]
+        let selection = Binding<String?>(wrappedValue: "Iron Man")
+        let sut = OptionalMenuField(selection: selection, options: options, placeholder: "Choose one").environmentObject(Theme())
+        
+        // then
+        let selectionLabelString = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).string()
+        XCTAssertEqual(selectionLabelString, "Iron Man")
+    }
+    
+    func test_optionalMenuField_givenSelection_whenSelectionChanged_thenCorrectlyUpdatesLabel() throws {
+        // given
+        let options = ["Thor", "Iron Man", "Captain America"]
+        let selection = Binding<String?>(wrappedValue: "Iron Man")
+        let sut = OptionalMenuField(selection: selection, options: options).environmentObject(Theme())
+
+        // when
+        try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().picker(0).select(value: Optional("Thor"))
+        
+        // then
+        let selectionLabelString = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).string()
+        XCTAssertEqual(selectionLabelString, "Thor")
+    }
+    
+    func test_optionalMenuField_givenOptionalMenuFieldWithNonNilSelection_thenLabelIsOfInputTypography() throws {
+        // given
+        let options = ["Thor", "Iron Man", "Captain America"]
+        let selection = Binding<String?>(wrappedValue: "Iron Man")
+        let sut = OptionalMenuField(selection: selection, options: options).environmentObject(Theme())
+        
+        // then
+        let actualAttributes = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).attributes()
+        let expectedAttributes = try Text("").typography(.input).environmentObject(Theme()).inspect().text(0).attributes()
+        XCTAssertEqual(try actualAttributes.font(), try expectedAttributes.font())
+    }
+    
+    func test_optionalMenuField_givenOptionalMenuFieldWithNilSelection_thenLabelIsOfEmptyFieldTypography() throws {
+        // given
+        let options = ["Thor", "Iron Man", "Captain America"]
+        let selection = Binding<String?>(wrappedValue: nil)
+        let sut = OptionalMenuField(selection: selection, options: options).environmentObject(Theme())
+        
+        // then
+        let actualAttributes = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).attributes()
+        let expectedAttributes = try Text("").typography(.emptyField).environmentObject(Theme()).inspect().text(0).attributes()
+        XCTAssertEqual(try actualAttributes.font(), try expectedAttributes.font())
+    }
+    
+    func test_optionalMenuField_givenSelectionNil_whenSelectionChanged_thenCorrectlyUpdatesTypograhy() throws {
+        // given
+        let options = ["Thor", "Iron Man", "Captain America"]
+        let selection = Binding<String?>(wrappedValue: nil)
+        let sut = OptionalMenuField(selection: selection, options: options).environmentObject(Theme())
+        
+        var actualAttributes = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).attributes()
+        var expectedAttributes = try Text("").typography(.emptyField).environmentObject(Theme()).inspect().text(0).attributes()
+        XCTAssertEqual(try actualAttributes.font(), try expectedAttributes.font())
+
+        // when
+        try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().picker(0).select(value: Optional("Captain America"))
+        
+        // then
+        actualAttributes = try sut.inspect().view(OptionalMenuField<Swift.String>.self).menu().labelView().hStack().text(0).attributes()
+        expectedAttributes = try Text("").typography(.input).environmentObject(Theme()).inspect().text(0).attributes()
         XCTAssertEqual(try actualAttributes.font(), try expectedAttributes.font())
     }
 }
