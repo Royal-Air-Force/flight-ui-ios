23d85d6e226e2bad224980df184edde893670773
Kaiman Mehmet <kaiman.mehmet@accenture.com>
2023-01-20 11:02:28 +0000

    Added some view inspector tests for input field

---
+++ Tests/FlightUITests/Components/InputFieldTests.swift
@@ -0,0 +1,67 @@
+import XCTest
+import SwiftUI
+
+import ViewInspector
+
+@testable import FlightUI
+
+class InputFieldTests: XCTestCase {
+
+    // MARK: - Input Field -
+
+    func test_inputField_bindingWorksCorrectly() throws {
+        // given
+        let text = Binding<String>(wrappedValue: "")
+        let inputField = InputField("",
+                                    text: text,
+                                    of: .decimal).environmentObject(Theme())
+
+        // when
+        try inputField.inspect().view(InputField.self).zStack().textField(0).setInput("Test Binding")
+
+        // then
+        XCTAssertEqual(text.wrappedValue, "Test Binding")
+    }
+
+    func test_inputField_noThemeStylingBindingWorksCorrectly() throws {
+        // given
+        let text = Binding<String>(wrappedValue: "")
+        let inputField = InputField("",
+                                    text: text,
+                                    of: .decimal,
+                                    useThemeStyling: false).environmentObject(Theme())
+
+        // when
+        try inputField.inspect().view(InputField.self).zStack().textField(0).setInput("Test Binding")
+
+        // then
+        XCTAssertEqual(text.wrappedValue, "Test Binding")
+    }
+
+    func test_inputField_givenFormatter_whenInputEntered_thenFormatsCorrectly() throws {
+        // given
+        let text = Binding<String>(wrappedValue: "")
+        let inputField = InputField("",
+                                    text: text,
+                                    formatter: .decimal(maximumIntegerDigits: 42, maximumFractionDigits: 1),
+                                    of: .decimal).environmentObject(Theme())
+
+        // when
+        try inputField.inspect().view(InputField.self).zStack().textField(0).setInput("123.123")
+
+        // then
+        XCTAssertEqual(text.wrappedValue, "123.1")
+    }
+
+    func test_inputField_givenUnformmatedString_thenShowsFormattedString() throws {
+        // given
+        let text = Binding<String>(wrappedValue: "543.21")
+        let inputField = InputField("",
+                                    text: text,
+                                    formatter: .decimal(maximumIntegerDigits: 42, maximumFractionDigits: 1),
+                                    of: .decimal).environmentObject(Theme())
+
+        // then
+        XCTAssertEqual(try inputField.inspect().view(InputField.self).zStack().textField(0).input(), "543.2")
+    }
+}
