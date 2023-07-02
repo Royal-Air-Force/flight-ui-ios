0bc49adfa6fa8fdfd4820b91a7d5bcd5cb19e73d
David Jones <97163363+DavidJonesIW@users.noreply.github.com>
2023-07-02 15:30:06 +0100

    ASTRA-3604 Fixing incorrect light theme defaults

--- Sources/FlightUI/Theme/Theme+Extensions.swift
+++ Sources/FlightUI/Theme/Theme+Extensions.swift
@@ -12,13 +12,13 @@ extension Theme {
     )
 
     public static let light = Theme(
-        color: ThemeColors(primary: .flightWhite,
-                           onPrimary: ColorState(color: .flightBlack),
-                           secondary: .flightLightGrey,
-                           onSecondary: ColorState(color: .flightBlack),
-                           background: .flightDarkGrey,
-                           onBackground: ColorState(color: .flightWhite),
-                           surface: .flightGrey,
-                           onSurface: ColorState(color: .flightWhite))
+        color: ThemeColors(primary: .flightBlack,
+                           onPrimary: ColorState(color: .flightWhite),
+                           secondary: .flightDarkGrey,
+                           onSecondary: ColorState(color: .flightWhite),
+                           background: .flightWhite,
+                           onBackground: ColorState(color: .flightBlack),
+                           surface: .flightLightGrey,
+                           onSurface: ColorState(color: .flightBlack))
     )
 }
