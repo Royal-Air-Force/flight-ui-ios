f55037869652c4b235866ad0d5a5070c45c5b8bb
David Jones <97163363+DavidJonesIW@users.noreply.github.com>
2023-07-02 19:06:38 +0100

    ASTRA-3590 Adding outline button style

--- Sources/FlightUI/Styles/ButtonStyle.swift
+++ Sources/FlightUI/Styles/ButtonStyle.swift
@@ -21,14 +21,26 @@ public struct FilledButtonStyle: ButtonStyle {
     }
 }
 
-private extension ColorState {
-    func getColorForState(disabled: Bool, focused: Bool) -> Color {
-        if (disabled) {
-            return self.disabledColor
-        } else if (focused) {
-            return self.focusedColor
-        } else {
-            return self.default
-        }
+public struct OutlineButtonStyle: ButtonStyle {
+    @EnvironmentObject var theme: Theme
+    @Environment(\.isEnabled) private var isEnabled: Bool
+    @Environment(\.isFocused) private var isFocused: Bool
+    
+    public init() {}
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding([.leading, .trailing], theme.padding.grid2x)
+            .frame(minHeight: theme.size.medium)
+            .foregroundColor(theme.color.nominal.getColorForState(disabled: !isEnabled, focused: isFocused))
+            .opacity(configuration.isPressed ? 0.6 : 1.0)
+            .scaleEffect(configuration.isPressed ? 1.1 : 1.0)
+            .fontWeight(.semibold)
+            .fontStyle(theme.font.body)
+            .clipShape(Capsule())
+            .overlay(
+                Capsule(style: .circular)
+                    .strokeBorder(theme.color.nominal.getColorForState(disabled: !isEnabled, focused: isFocused), style: StrokeStyle(lineWidth: theme.size.border))
+            )
     }
 }
--- Sources/FlightUI/Theme/ThemeButtons.swift
+++ Sources/FlightUI/Theme/ThemeButtons.swift
@@ -2,8 +2,12 @@ import SwiftUI
 
 public class ThemeButtons {
     @Published public var filled: FilledButtonStyle
+    @Published public var outline: OutlineButtonStyle
     
-    public init(filled: FilledButtonStyle = FilledButtonStyle()) {
+    public init(filled: FilledButtonStyle = FilledButtonStyle(),
+                outline: OutlineButtonStyle = OutlineButtonStyle()
+    ) {
         self.filled = filled
+        self.outline = outline
     }
 }
