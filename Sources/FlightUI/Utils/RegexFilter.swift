f36d168ba34e9e92e81013b3ffdaf9bdc1f2c809
Lawrence Reynolds <lawrence@appivate.mod.uk>
2026-08-28 11:13:46 +0100

    fix regex filter

--- Sources/FlightUI/Utils/RegexFilter.swift
+++ Sources/FlightUI/Utils/RegexFilter.swift
@@ -12,13 +12,13 @@ public enum RegexFilter {
     public var regex: String {
         switch self {
         case .integerOnly:
-            return "[^0-9]"
-        case .doubleOnly:
-            return "[^0-9.]"
-        case .letterOnly:
-            return "[^A-Z^a-z]"
-        case .noDigits:
             return "[0-9]"
+        case .doubleOnly:
+            return "[0-9.]"
+        case .letterOnly:
+            return "[A-Za-z]"
+        case .noDigits:
+            return "[^0-9]"
         case .custom(let customValue):
             return customValue
         }
